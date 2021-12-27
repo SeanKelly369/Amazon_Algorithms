8d4237dda0094af05d1aa70d18f5bb22a8a2ab9a
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-12-27 11:44:22 +0000

    Update SumTwoLists.java

--- SumTwoLists.java
+++ SumTwoLists.java
@@ -1,7 +1,7 @@
 
- // Solution that is not accepted on Leetcode
+// Solution that is not accepted on Leetcode
 
-* Definition for singly-linked list.
+/* Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
@@ -43,4 +43,41 @@ class SumTwoLists {
         }
         return dummyHead.next;
     }
-}}
+}
+
+
+class AddTwoNumberFromList {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        ListNode dummyHead = new ListNode(-1);
+        ListNode l3 = dummyHead;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int l1Val = (l1 != null) ? l1.val : 0;
+            int l2Val = (l2 != null) ? l2.val : 0;
+            int sum = l1Val + l2Val + carry;
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            ListNode newNode = new ListNode(sum);
+            l3.next = newNode;
+            l3 = l3.next;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        
+        if(carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            l3.next = newNode;
+            l3 = l3.next;
+
+        }
+
+        return dummyHead.next;
+    }
+}
