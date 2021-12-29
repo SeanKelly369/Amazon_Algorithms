3bf7cc763c510fb26dbb5da3d789888b417ba88c
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-12-29 00:37:29 +0000

    Update CountUniqueCharactersOfAllSubstringsOfGivenString.java

--- CountUniqueCharactersOfAllSubstringsOfGivenString.java
+++ CountUniqueCharactersOfAllSubstringsOfGivenString.java
@@ -3,24 +3,24 @@
 class CountUniqueCharactersOfAllSubstringsOfGivenString {
     public int uniqueLetterString(String s) {
         
-	int[] lastSeen = new int[26]; // cover all the letters of the alphabet
-        Arrays.fill(lastSeen, -1);
+	int[] lastSeenCharIndexPositions = new int[26]; // cover all the letters of the alphabet
+        Arrays.fill(lastSeenCharIndexPositions, -1); 
         
-        int[] preLastSeen = new int[26];
-        Arrays.fill(preLastSeen, -1);
+        int[] charOccurrenceCounter = new int[26]; // Count the number of times a letter in the alphabet occurs
+        Arrays.fill(charOccurrenceCounter, -1);
 		
 	int count = 0; 
         int res = 0;
 	
 	for (int i = 0; i < s.length(); i++) {
             int charValue = s.charAt(i) - 'A'; // All characters are uppercase
-            int lastVisitedCharValue = lastSeen[charValue];
-            int preLastVisitedCharValue = preLastSeen[charValue];
+            int lastVisitedCharValue = lastSeenCharIndexPositions[charValue];
+            int preLastVisitedCharValue = charOccurrenceCounter[charValue];
 			
             // -- double minus becomes a plus.  Ingenious code
             count += i - lastVisitedCharValue - lastVisitedCharValue + preLastVisitedCharValue;     
-            preLastSeen[charValue] = lastSeen[charValue];
-            lastSeen[charValue] = i;
+            charOccurrenceCounter[charValue] = lastSeenCharIndexPositions[charValue];
+            lastSeenCharIndexPositions[charValue] = i;
             res += count;   
 	}        
 	return res;
