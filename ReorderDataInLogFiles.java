22805c39463fe3985d1c78270ea4f18f9ce841d5
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-12-30 00:14:20 +0000

    Create ReorderDataInLogFiles.java

---
+++ ReorderDataInLogFiles.java
@@ -0,0 +1,37 @@
+class ReorderDataInLogFiles {
+    /*
+      Grab all letter logs
+      sort them by substring, or identifier
+      then put all digit logs after
+    */
+    public String[] reorderLogFiles(String[] logs) {
+        List<String> letterLogs = new ArrayList<String>(logs.length);
+        List<String> digitLogs = new ArrayList<String>(logs.length);
+        for(String log : logs) {
+          if(isLetterLog(log)) {              
+            letterLogs.add(log);
+          } else {              
+            digitLogs.add(log);
+          }
+        }
+        
+        Collections.sort(letterLogs, (a,b) -> {
+          String aLog = a.substring(a.indexOf(" "));
+          String bLog = b.substring(b.indexOf(" "));
+            
+          if(aLog.equals(bLog)) {              
+            return a.compareTo(b);
+          } else {              
+            return aLog.compareTo(bLog);
+          }
+        });
+        
+      letterLogs.addAll(digitLogs);
+      return letterLogs.toArray(new String[letterLogs.size()]);
+    }
+  
+    private boolean isLetterLog(String log){
+        Character character = log.charAt(log.indexOf(" ") + 1);
+      return Character.isLetter(character);
+    }
+}
